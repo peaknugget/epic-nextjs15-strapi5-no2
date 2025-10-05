08a5299db06478fd7aefd27a704d3e9541f65e84
braverokmc79 <braverokmc79@gmail.com>
2025-10-05 22:34:51 +0900

    move blog

---
+++ frontend/App.tsx
@@ -0,0 +1,6 @@
+// This file has been moved to preserve Next.js 15 App Router functionality
+// All routing is now handled by files in the /app directory
+
+export default function DisabledApp() {
+  return null;
+}
